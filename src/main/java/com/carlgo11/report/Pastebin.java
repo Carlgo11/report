51b19f7156423b71836c2f30a990b932a9a2ef18
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-12-27 00:11:16 +0100

    Rework exception handling & add more javadoc documentation

--- src/main/java/com/carlgo11/report/Pastebin.java
+++ src/main/java/com/carlgo11/report/Pastebin.java
@@ -1,5 +1,6 @@
 package com.carlgo11.report;
 
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -7,31 +8,47 @@ import java.net.URLEncoder;
 
 public class Pastebin {
 
-    private String api_user_key = ""; //Insert your own api_user_key if you have one.
-    private String api_dev_key = "";
+    private String api_user_key;
+    private String api_dev_key;
 
+    /**
+     *
+     * @param api_user_key Pastebin API_USER_KEY to use. Can be left blank
+     * @param api_dev_key  Pastebin API_DEV_KEY to use.
+     * @since 1.1
+     */
     public Pastebin(String api_user_key, String api_dev_key) {
         this.api_user_key = api_user_key;
         this.api_dev_key = api_dev_key;
     }
 
-    private String checkResponse(String response) {
-        if (response.substring(0, 15).equals("Bad API request")) {
-            return response.substring(17);
-        }
-        return "";
+    private boolean checkResponse(String response) {
+        return response.substring(0, 15).equals("Bad API request");
     }
 
+    /**
+     * Posts a new Pastebin paste.
+     *
+     * @param name Name of the paste
+     * @param body Content of the paste
+     * @return Returns a string containing the URL of the paste if successful, otherwise NULL.
+     * @throws UnsupportedEncodingException Throws unsupported encoding exception if the body or name can't be URL encoded.
+     * @since 1.0
+     */
     public String makePaste(String name, String body) throws UnsupportedEncodingException {
         String content = URLEncoder.encode(body, "UTF-8");
         String title = URLEncoder.encode(name + " report", "UTF-8");
         String data = String.format("api_option=paste&api_user_key=%s&api_paste_private=0&api_paste_name=%s&api_paste_expire_date=1M&api_paste_format=text&api_dev_key=%s&api_paste_code=%s", this.api_user_key, title, this.api_dev_key, content);
         String pasteURL = "http://www.pastebin.com/api/api_post.php";
         String response = this.page(pasteURL, data);
-        assert response != null;
-        String check = this.checkResponse(response);
-        if (!check.equals("")) {
-            return check;
+        if (response == null) {
+            Plugin.sendError("No response from Pasebin");
+            return null;
+        }
+        boolean check = this.checkResponse(response);
+        if (check) {
+            Plugin.sendError("Bad API Request.");
+            return null;
         }
         return response;
     }
@@ -62,7 +79,7 @@ public class Pastebin {
             InputStream is = connection.getInputStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
             String line;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
             while ((line = rd.readLine()) != null) {
                 response.append(line);
             }
--- src/main/java/com/carlgo11/report/Plugin.java
+++ src/main/java/com/carlgo11/report/Plugin.java
@@ -1,8 +1,22 @@
 package com.carlgo11.report;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends JavaPlugin {
+
+    /**
+     * Send an error message to the console.
+     *
+     * @param message Message to be displayed.
+     */
+    public static void sendError(String message) {
+        Logger logger = Bukkit.getLogger();
+        logger.log(Level.WARNING, message);
+    }
 
     @Override
     public void onEnable() {
